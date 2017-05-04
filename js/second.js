f285e28b898b1cdb2eaefc679732d024e03fb445
Kotavy <tavius.koktavy@gmail.com>
2017-05-04 00:46:56 -0400

    Progress Backup
    Before I get too lost trying to combine PointerLockControls with
    raycasting to detect wall clicks.

--- js/second.js
+++ js/second.js
@@ -7,8 +7,10 @@
 // NOTE: On click of the buddha walls, they "fall over" (transform flat)
 
 // Global variables:
-var camera, scene, renderer, controls;
+var camera, scene, raycaster, renderer, controls;
 var skyboxGeometry, skyboxMaterial, skyboxMesh;
+var fakePlane, fakeMaterial, fakeMesh;
+var fakeMaterialArray = ['neg-z', 'pos-x', 'pos-z', 'neg-x', 'pos-y', 'pos-z'];
 var width = window.innerWidth;
 var height = window.innerHeight;
 var container = document.getElementById('container');
@@ -16,15 +18,19 @@ var blocker = document.getElementById('blocker');
 var instructions = document.getElementById('instructions');
 var havePointerLock = 'pointerLockElement' in document;
 
+var mouse = new THREE.Vector2(), INTERSECTED;
+
+
 // Page listener:
 window.addEventListener('resize', onWindowResize);
+// window.addEventListener('mousemove', onMouseMove);
 window.addEventListener('click', onClick);
 
 function init() {
   // Create scene and camera:
   scene = new THREE.Scene();
   camera = new THREE.PerspectiveCamera(90, width/height, 0.1, 25000);
-  camera.position.set(0, 3, 8);
+  camera.position.set(0, 1, 1);
   scene.add(camera);
 
   // Skybox
@@ -37,28 +43,80 @@ function init() {
     scene.add(skyboxMesh);
   });
 
-  //NOTE: Add look controls here.
-  //NOTE: NO CLICK
+  // Fake Environment
+  var textureLoader = new THREE.TextureLoader();
+  textureLoader.load('images/buddha/neg-z.jpg', function(tex) {
+    fakeMaterial = new THREE.MeshBasicMaterial({map: tex, side: THREE.DoubleSide});
+    fakePlane = new THREE.PlaneGeometry(1000, 1000, 10, 10);
+    fakeMesh = new THREE.Mesh(fakePlane, fakeMaterial);
+    fakeMesh.position.z = -500;
+    scene.add(fakeMesh);
+    fakeMesh.addEventListener('click', function(){ wallFall(fakeMesh) }, false);
+  });
+  var textureLoader = new THREE.TextureLoader();
+  textureLoader.load('images/buddha/pos-x.jpg', function(tex) {
+    fakeMaterial = new THREE.MeshBasicMaterial({map: tex, side: THREE.DoubleSide});
+    fakePlane = new THREE.PlaneGeometry(1000, 1000, 10, 10);
+    fakeMesh = new THREE.Mesh(fakePlane, fakeMaterial);
+    fakeMesh.position.x = -500;
+    fakeMesh.rotation.y = Math.PI / 2;
+    scene.add(fakeMesh);
+  });
+  var textureLoader = new THREE.TextureLoader();
+  textureLoader.load('images/buddha/pos-z.jpg', function(tex) {
+    fakeMaterial = new THREE.MeshBasicMaterial({map: tex, side: THREE.DoubleSide});
+    fakePlane = new THREE.PlaneGeometry(1000, 1000, 10, 10);
+    fakeMesh = new THREE.Mesh(fakePlane, fakeMaterial);
+    fakeMesh.position.z = 500;
+    fakeMesh.rotation.y = Math.PI;
+    scene.add(fakeMesh);
+  });
+  var textureLoader = new THREE.TextureLoader();
+  textureLoader.load('images/buddha/neg-x.jpg', function(tex) {
+    fakeMaterial = new THREE.MeshBasicMaterial({map: tex, side: THREE.DoubleSide});
+    fakePlane = new THREE.PlaneGeometry(1000, 1000, 10, 10);
+    fakeMesh = new THREE.Mesh(fakePlane, fakeMaterial);
+    fakeMesh.position.x = 500;
+    fakeMesh.rotation.y = Math.PI / -2;
+    scene.add(fakeMesh);
+  });
+  var textureLoader = new THREE.TextureLoader();
+  textureLoader.load('images/buddha/pos-y.jpg', function(tex) {
+    fakeMaterial = new THREE.MeshBasicMaterial({map: tex, side: THREE.DoubleSide});
+    fakePlane = new THREE.PlaneGeometry(1000, 1000, 10, 10);
+    fakeMesh = new THREE.Mesh(fakePlane, fakeMaterial);
+    fakeMesh.position.y = 500;
+    fakeMesh.rotation.x = Math.PI / -2;
+    fakeMesh.rotation.y = Math.PI;
+    scene.add(fakeMesh);
+  });
+  var textureLoader = new THREE.TextureLoader();
+  textureLoader.load('images/buddha/neg-y.jpg', function(tex) {
+    fakeMaterial = new THREE.MeshBasicMaterial({map: tex, side: THREE.DoubleSide});
+    fakePlane = new THREE.PlaneGeometry(1000, 1000, 10, 10);
+    fakeMesh = new THREE.Mesh(fakePlane, fakeMaterial);
+    fakeMesh.position.y = -500;
+    fakeMesh.rotation.x = Math.PI / -2;
+    fakeMesh.rotation.z = Math.PI;
+    scene.add(fakeMesh);
+  });
 
-  // Create renderer:
-  renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true});
+  // Create renderer and controls:
+  raycaster = new THREE.Raycaster();
+  renderer = new THREE.WebGLRenderer();
   renderer.setSize(width, height);
-
 	controls = new THREE.PointerLockControls(camera, renderer.domElement);
 	scene.add(controls.getObject());
-	// container.addEventListener('click', function(event) {
-  // 	controls.enabled = true;
-	// 	 container.requestPointerLock();
-	//  });
+  raycaster.setFromCamera( mouse, camera );
+  console.log(raycaster);
   controls.enablePan = false;
   controls.enableZoom = false;
-
   container.appendChild(renderer.domElement);
 }
 
 function animate() {
   requestAnimationFrame(animate);
-  renderer.render(scene, camera);
+	renderer.render(scene, camera);
 }
 
 // Update width and height, then adjust the camera and re-render:
@@ -77,5 +135,37 @@ function onClick() {
 	blocker.style.display = 'none';
 }
 
+function wallFall(wall) {
+  wall.position.y = -500;
+  wall.rotation.x = Math.PI / 2;
+}
+
 init();
 animate();
+
+
+
+// Fake Environment loop-loader WIP:
+
+  // for (var i = 0; i < fakeMaterialArray.length; i++) {
+  //   var textureLoader = new THREE.TextureLoader();
+  //   textureLoader.load('images/buddha/' + fakeMaterialArray[i] + '.jpg', function(tex) {
+  //     fakeMaterial = new THREE.MeshBasicMaterial({map: tex, side: THREE.DoubleSide});
+  //     fakePlane = new THREE.PlaneGeometry(1000, 1000, 10, 10);
+  //     fakeMesh = new THREE.Mesh(fakePlane, fakeMaterial);
+  //     if (i = 0) {
+  //       fakeMesh.position.z = -500;
+  //     }
+  //     if (i = 1) {
+  //     }
+  //     if (i = 2) {
+  //     }
+  //     if (i = 3) {
+  //     }
+  //     if (i = 4) {
+  //     }
+  //     if (i = 5) {
+  //     }
+  //     scene.add(fakeMesh);
+  //   })
+  // };
